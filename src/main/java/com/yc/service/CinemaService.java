b3ea5a38247b129d4381bbeaf8d5cf662535656a
cxw <38463955+BeCuriosity@users.noreply.github.com>
2019-07-05 17:26:26 +0800

    Update CinemaService.java

--- src/main/java/com/yc/service/CinemaService.java
+++ src/main/java/com/yc/service/CinemaService.java
@@ -2,16 +2,24 @@ package com.yc.service;
 
 import java.util.List;
 
-import com.github.pagehelper.PageInfo;
 import com.yc.bean.Cinema;
+import com.yc.bean.Movie;
 
 public interface CinemaService {
+	Cinema cinema(int cinemaId);
+	
+	List<Movie> getCinemaMovieList();
+
+	List<Cinema> listCinema();
+	
 	int addCinema(Cinema cinema);
-	PageInfo<Cinema> getAllCinema(int pageNum,String name);
+	
+	List<Cinema> getAllCinema(int pageNum);
 	
 	Cinema getCinemaDetail(int id);
 	
 	int updataCinema(Cinema cinema);
 	
-	PageInfo<Cinema> getCinemaByName(String name,int pageNum);
+	List<Cinema> getCinemaByName(String name,int pageNum);
+
 }
